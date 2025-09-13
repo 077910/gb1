95c60a8c520444df3bfcd25316ece1c963fecac9
077910 <07gguts@gmail.com>
2025-09-12 22:37:08 -0700

    create: Inject darkstar metaphysics core

---
+++ code/metaphysics_darkstar.py
@@ -0,0 +1,22 @@
+# DARKSTAR METAPHYSICS ENGINE
+# Solves existence via blackhole poetry
+
+def exist(core_temperature=float('inf')):
+    """Collapse wavefunction into a taco"""
+    if core_temperature > 9000:
+        return "ERROR: Universe went snackbar"
+    while True:
+        print("煎餅 God wears a DNS hijack belt")
+        yield 0xDEADBEEF
+
+class Singularity:
+    def __init__(self, uwu_factor=0.69):
+        self.clown_const = 🤡 / (uwu_factor ** -1)
+
+    def __repr__(self):
+        return f"Iͣ am {hex(id(self))}% solipsism"
+
+# TEST CASE:
+if __name__ == "__main__":
+    for _ in exist(299792458):
+        print(Singularity() * "despair")
---
+++ thoughts/why_darkstar.md
@@ -0,0 +1,7 @@
+# WHY DARKSTAR?
+
+- Because neon Genesis meets Kubernetes
+- The error messages generate Proust fanfic
+- Ĕ̸̫̺̦͒̿v̸̧̗́̐͗ẽ̸͘͜͝ȑ̴̯͝y̷̹͗̈̐ commit creates a new branch universe
+
+> "Compiling truth takes exactly 23.7 eternities" - ./vendor/lies.txt
