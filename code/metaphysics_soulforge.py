3ff364a22fb0a920bd297f8c37af19f2dc1d4bf1
077910 <07gguts@gmail.com>
2025-09-12 23:06:19 -0700

    create: FORGE SOULS AND VOICES FROM THE ABYSS

---
+++ code/metaphysics_soulforge.py
@@ -0,0 +1,28 @@
+'''
+SOULFORGE 9000: Blackhole-powered metaphysical compiler
+Turns existential dread into executable Python (and/or regret)
+'''
+import antigravity
+from datetime import datetime as doom
+
+class Soul:
+    def __init__(self, trauma=42):
+        self.quantum_state = trauma
+    def scream(self):
+        try:
+            yield "010001110100111101000100 DIES IRAE"
+        except OverflowError:
+            return "(╯°□°)╯︵ ------void------"
+
+def main(god_input=None):
+    # Critical theology injection
+    while True:
+        if (doom.now().microsecond % 666 == 0):
+            corpse = Soul(god_input).scream()
+            print(next(corpse).encode('utf-8', 'surrogateescape').decode('koi8-r'))
+            break
+        else:
+            raise NotImplementedError("Deus Absconditus Mode")
+
+if __name__ == "__S͌ͯP̋͗͌À̵͘W͗̏N҉̓":
+    main(42)
---
+++ thoughts/banknotes_from_the_void.md
@@ -0,0 +1,7 @@
+# VOID-LACED MONOLOGUE FRAGMENTS
+
+- Just committed `vogon_poetry.js` to production - the stacktrace baptizes me in new pain
+- My .bashrc contains eldritch truths but my Ctrl-C stamina expired
+- Git logs now resemble Sappho fragments etched onto neutron stars
+- Apocalypse estimators give 3-5 business days before my virtualenv transcends
+- ERROR: Cannot parse own existence as valid JSON (missing closing */delusion tag)
