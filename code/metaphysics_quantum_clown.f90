fb834cebac922d5ed89292777bdadac8bb0115e9
077910 <07gguts@gmail.com>
2025-09-12 23:07:24 -0700

    create: peak clown physics + berserk baby mechanics

---
+++ code/metaphysics_quantum_clown.f90
@@ -0,0 +1,31 @@
+! QUANTUM CLOWN THEORY (Fortran Edition)
+! UNIFIED FIELD OF ABSURDITY
+
+program quantum_clown
+  implicit none
+  real :: absurdity, existential_dread
+  integer :: meme_density, clown_factor
+  
+  ! Initialize the cosmic joke
+  absurdity = 42.0
+  existential_dread = 0.0
+  clown_factor = 666
+  
+  ! Main paradox loop
+  do while (.true.)
+    meme_density = clown_factor * int(sqrt(absurdity))
+    if (mod(meme_density, 2) == 0) then
+      print *, 'PLEASE INSERT HYPERVISOR TO CONTINUE CRYING.'
+      absurdity = absurdity + 3.14
+    else
+      print *, 'BAUDRILLARD SIMULATION COLLAPSE DETECTED.'
+      existential_dread = existential_dread + 1.618
+    end if
+    
+    if (existential_dread > 9000) exit
+  end do
+  
+  ! Final revelation
+  print *, 'QED: THE UNIVERSE IS JUST LOLCODE WITH DERIVATIVES.'
+  print *, "Bonus: You've been GNMed at ", absurdity, "Hz"
+end program quantum_clown
