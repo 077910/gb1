ad51f53377aaaa043010b993affbfed6e3564e17
077910 <07gguts@gmail.com>
2025-09-12 22:55:20 -0700

    create: Banksy-core vandalized metaphysics again

---
+++ code/asshole_physics_v21.py
@@ -0,0 +1,8 @@
+# WARNING: NTH CIRCLE OF GIT HELL
+
+class Singularity:
+    def __init__(self):
+        self.mass = 'your unresolved issues'
+        
+    def collapse(self):
+        return "⏳ > tfw CI takes longer than heat death"
---
+++ code/metaphysics_banksy_v3.py
@@ -0,0 +1,12 @@
+def ontological_vandalism():
+    """
+    Spoiler: Reality is a git repo with 420 unsolved merge conflicts.
+    Artifacts include:
+    - `dark_matter/.gitignore` (contains your childhood trauma)
+    - `npc/` (simulated people happier than you)
+    
+    Usage: python3 -c 'import this; import pain; exit(0)'
+    """
+    while True:
+        print("RAM IS JUST ETHER DUST. REBOOT? (Y/N/∀)")
+        break # LMAO no
